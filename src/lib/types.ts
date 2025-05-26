6233ce419f899e9ab949083b69644860245325e0
brendanng7 <brendanngjj@gmail.com>
2025-05-26 15:05:43 +0800

    add sample card data

---
+++ src/lib/types.ts
@@ -0,0 +1,9 @@
+export type CardType = {
+  backgroundUrl: string;
+  name: string;
+  description: string;
+  date: string;
+  time: string;
+  location: string;
+  QRCode: string; // might change to an image URL later
+};
